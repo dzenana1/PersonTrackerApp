cc4863c01df4d0669db3eb80ac947990cbb3b838
Dzenana <dkapetanov1@etf.unsa.ba>
2016-12-26 16:48:11 +0100

    komentarisanje haman gotovo

--- PersonTrackerApplication/PersonTrackerApplication/Controllers/homePretragaController.js
+++ PersonTrackerApplication/PersonTrackerApplication/Controllers/homePretragaController.js
@@ -2,7 +2,7 @@
     return {
         restrict: 'M',
         link: function (scope, elem, attr) {
-            alert(attr.myDirectiveName); //alerts "this is all an argument"
+            alert(attr.myDirectiveName); 
         }
     };
 });
@@ -13,15 +13,18 @@ mainModule.controller('homePretragaController', ['$scope', '$http', '$log', func
     var serviceBase = "http://localhost:4094/";
     $scope.listaNestalih = [];
 
-    $scope.btn_add = function (txtcomment, imeKorisnika, prezimeKorisnika, emailKorisnika, idNestali) {
-        if ($scope.txtcomment != '') {
-            //$http.post(serviceBase + 'api/Komentar/RegisterCom?komentar=' + txtcomment + '&ime=' + imeKorisnika + '&prezime=' + prezimeKorisnika + '&email=' + emailKorisnika + '&idNestali=' + idNestali)
-            //    .success(function (data) {
-            //        $scope.greeting = data;
-            //    })
 
-            $scope.txtcomment = "";
-            console.log("nss")
+    $scope.btn_add = function (index,txtcomment,imeKorisnika,prezimeKorisnika,emailKorisnika,idNestali) {
+        console.log(index)
+        console.log(txtcomment)
+
+        if (txtcomment != '') {
+            $http.post(serviceBase + 'api/Komentar/RegisterCom?komentar=' + txtcomment + '&ime=' + imeKorisnika + '&prezime=' + prezimeKorisnika + '&email=' + emailKorisnika + '&idNestali=' + idNestali)
+                .success(function (data) {
+                    $scope.greeting = data;
+                    $scope.listaNestalih[index].listaKomentara.push(txtcomment);
+
+                })
         }
 
     }
@@ -31,9 +34,8 @@ mainModule.controller('homePretragaController', ['$scope', '$http', '$log', func
         $http.get(serviceBase + 'api/Pretraga/Pretrazi?parametarPretrage=' + $scope.parametarPretrage)
         .success(function (data) {
             $scope.listaNestalih = data;
-        }
-        )
-
+          //  $scope.listaNestalih2 = data;
+        })
     }
 
 
